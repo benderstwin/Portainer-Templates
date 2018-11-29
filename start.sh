f721dd5e7549a31386a8153c199beb0f109e4615
benderstwin <bryan@bwirth.com>
2018-11-29 15:29:39 -0700

    update files

---
+++ start.sh
@@ -0,0 +1,1 @@
+docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v ${DOCKERDIR}/portainer/templates.json:/templates.json -v ${DOCKERDIR}/portainer/data:/data portainer/portainer 
